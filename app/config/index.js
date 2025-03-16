cb585f320d8505e9eeabb8dbf1c00f7377766b3e
tinhto123 <Tinhck23v7k532@vlvh.ctu.edu.vn>
2025-03-16 09:28:27 +0700

    Cai dat cac handler truy xuat CSDL

--- app/config/index.js
+++ app/config/index.js
@@ -2,5 +2,10 @@ const config = {
   app: {
     port: process.env.PORT || 3000,
   },
+  db: {
+    uri:
+      "mongodb://127.0.0.1:27017/contacts" ||
+      "mongodb://127.0.0.1:27017/contactbook",
+  },
 };
 module.exports = config;
